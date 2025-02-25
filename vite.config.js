08e633fb9abcfc0dec75520461d7429c7f92ab1a
BenjaminPham116 <benjaminqd.pham@gmail.com>
2025-02-24 23:58:46 -0600

    updated config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import vitePluginString from 'vite-plugin-string'
 
 export default {
+  base: process.env.NODE_ENV === 'production' ? '/threejs-pond/' : '',
   plugins: [
     vitePluginString()
   ]
